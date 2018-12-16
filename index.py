7bf5e78b813131c75fe0cee62cbac0b49dbf8720
Michal Marchewka <veenkar@gmail.com>
2018-12-16 19:25:05 +0100

    full skip and include path functionalities, creates both .files and .includes, multiple project and defautl projects support

--- index.py
+++ index.py
@@ -3,19 +3,27 @@
 
 import glob
 from pathlib import Path
+import argparse
 
+## options
 INCLUDE_EXTENSIONS = [".h", ".hpp", ".include"]
 OTHER_EXTENSIONS = [".c", ".cpp", ".cc", ".define"]
+SKIP_PATHS = ["**/cuda/**"]
+SEARCH_PATHS = ["tensorflow/core/kernels", "tensorflow/core/util"]
+DEFAULT_PROJS=["tf"]
 
-EXTENSIONS = INCLUDE_EXTENSIONS + OTHER_EXTENSIONS;
+## options postprocess
+EXTENSIONS = INCLUDE_EXTENSIONS + OTHER_EXTENSIONS
+if not SEARCH_PATHS:
+    SEARCH_PATHS = [""]
 
-rootdir = Path(".")
-rootdir_abs=Path(".").absolute()
-print("ROOT: {0}".format(rootdir_abs))
-
-
-def findFiles(extensions, searchdir=Path("."), skipdirs=[]):
+def findFiles(extensions, searchdir=Path("."), rootdir=None, skip_paths=[]):
     searchdir = Path(searchdir)
+    if rootdir == None:
+        rootdir = searchdir
+
+    rootdir = Path(rootdir)
+    print(searchdir)
 
     if not isinstance(extensions, list):
         extensions = [extensions]
@@ -26,33 +34,43 @@ def findFiles(extensions, searchdir=Path("."), skipdirs=[]):
             extension = "." + extension
             pass
 
-        print(extension)
-        searchexpr = Path(searchdir) / "**" / "*{0}".format(extension)
+        #print(extension)
+        searchexpr =  "**/*{0}".format(extension)
         str_searchexpr = str(searchexpr)
 
-        new_paths_objs = [Path(strpath) for strpath in glob.glob(str_searchexpr, recursive=True)]
-
-        new_paths = []
-        for new_path_obj in new_paths_objs:
-            append = True
-            for skipdir in skipdirs:
-                if new_path_obj.match(skipdir):
-                    append = False
-            if append:
-                new_paths.append(str(new_path_obj))
-
+        new_paths = searchdir.glob(str_searchexpr)
+        new_paths = [path.relative_to(rootdir) for path in new_paths]
+        new_paths = skipPaths(new_paths, skip_paths)
         if new_paths:
             paths += new_paths
 
     paths.sort()
     return paths
 
+def toGlob(paths):
+    if isinstance(paths, list):
+        return [Path(path) for path in paths]
+    else:
+        return Path(paths)
+
+def skipPaths(paths, skiplist):
+    #paths = toGlob(paths)
+    new_paths = []
+    for path in paths:
+        append = True
+        for skip_path in skiplist:
+            if path.match(skip_path):
+                append = False
+        if append:
+            new_paths.append(str(path))
+    return new_paths
+
 def fileLocations(files):
     dirs = []
     for file in files:
         basename = str(Path(file).parent)
         if not basename in dirs:
-            print(basename)
+            #print(basename)
             dirs.append(basename)
     dirs.sort()
     return dirs
@@ -62,16 +80,51 @@ def listToFile(in_list, filename):
         for item in in_list:
             f.write("%s\n" % item)
 
-include_files = findFiles(INCLUDE_EXTENSIONS, skipdirs=["**/cuda/**"])
-other_files = findFiles(OTHER_EXTENSIONS)
 
-files = ["index.py"] + include_files + other_files
-files.sort()
+def processProj(proj_name):
+    ## projdir
+    projdir = Path(proj_name)
+    proj_name_abs = Path(projdir).absolute()
+    print("PROJ: {0}".format(proj_name_abs))
 
-include_dirs = fileLocations(include_files)
+    include_files = []
+    other_files = []
+    for search_path in SEARCH_PATHS:
+        include_files += findFiles(extensions=INCLUDE_EXTENSIONS, searchdir = projdir / search_path, rootdir = projdir, skip_paths=SKIP_PATHS)
+        other_files += findFiles(extensions=OTHER_EXTENSIONS, searchdir = projdir / search_path, rootdir = projdir, skip_paths=SKIP_PATHS)
+
+    files = ["index.py"] + include_files + other_files
+    files.sort()
+
+    include_dirs = fileLocations(include_files)
+
+    listToFile(files, str(Path(proj_name) / "{0}.files".format(proj_name)) )
+    listToFile(include_dirs, str(Path(proj_name) / "{0}.includes".format(proj_name)) )
+
+    print(len(files))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Indexer.')
+    parser.add_argument('proj_name', nargs="*")
+    args = parser.parse_args()
+
+    print("Passed args: {0}".format(args.proj_name))
+
+    if len(args.proj_name):
+        projs = args.proj_name
+    else:
+        print("using default projs: {0}".format(DEFAULT_PROJS))
+        projs = DEFAULT_PROJS
+
+    ## rootdir
+    rootdir = Path(".")
+    rootdir_abs=Path(".").absolute()
+    print("ROOT: {0}".format(rootdir_abs))
+
+    for proj_name in projs:
+        processProj(proj_name)
 
 
-listToFile(files, "indexer.files")
-listToFile(include_dirs, "indexer.includes")
 
-print(len(files))
+
+
