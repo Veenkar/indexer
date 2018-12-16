07f3e3c09f29db7ba20c822255db2a10b11207ce
Michal Marchewka <veenkar@gmail.com>
2018-12-16 18:26:56 +0100

    skipdirs

--- index.py
+++ index.py
@@ -14,7 +14,9 @@ rootdir_abs=Path(".").absolute()
 print("ROOT: {0}".format(rootdir_abs))
 
 
-def findFiles(extensions, searchdir="."):
+def findFiles(extensions, searchdir=Path("."), skipdirs=[]):
+    searchdir = Path(searchdir)
+
     if not isinstance(extensions, list):
         extensions = [extensions]
 
@@ -27,7 +29,18 @@ def findFiles(extensions, searchdir="."):
         print(extension)
         searchexpr = Path(searchdir) / "**" / "*{0}".format(extension)
         str_searchexpr = str(searchexpr)
-        new_paths = glob.glob(str_searchexpr, recursive=True)
+
+        new_paths_objs = [Path(strpath) for strpath in glob.glob(str_searchexpr, recursive=True)]
+
+        new_paths = []
+        for new_path_obj in new_paths_objs:
+            append = True
+            for skipdir in skipdirs:
+                if new_path_obj.match(skipdir):
+                    append = False
+            if append:
+                new_paths.append(str(new_path_obj))
+
         if new_paths:
             paths += new_paths
 
@@ -49,7 +62,7 @@ def listToFile(in_list, filename):
         for item in in_list:
             f.write("%s\n" % item)
 
-include_files = findFiles(INCLUDE_EXTENSIONS)
+include_files = findFiles(INCLUDE_EXTENSIONS, skipdirs=["**/cuda/**"])
 other_files = findFiles(OTHER_EXTENSIONS)
 
 files = ["index.py"] + include_files + other_files
