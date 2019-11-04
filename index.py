3704f621c5c8b84b4c16c6c8001001f952823d8a
Michal Marchewka <veenkar@gmail.com>
2019-11-04 20:51:16 +0100

    cleanup

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import glob
 from pathlib import Path
-import argparse
 import re
 
 PRINT_PATHS=False
@@ -160,47 +158,3 @@ class Indexer:
 
         print(len(files))
 
-
-# TEST
-if __name__ == "__main__":
-    ## options
-    INCLUDE_EXTENSIONS = [".h", ".hpp", ".include"]
-    OTHER_EXTENSIONS = [".c", ".cpp", ".cc", ".define", ".py"]
-    SKIP_PATHS = ["cuda"]
-    SEARCH_PATHS = ["tensorflow/core/kernels", "tensorflow/core/util"]
-    DEFAULT_PROJS = ["tf"]
-
-    ## options postprocess
-    EXTENSIONS = INCLUDE_EXTENSIONS + OTHER_EXTENSIONS
-    if not SEARCH_PATHS:
-        SEARCH_PATHS = [""]
-
-
-    parser = argparse.ArgumentParser(description="Indexer.")
-    parser.add_argument("proj_name", nargs="*")
-    args = parser.parse_args()
-
-    print("Passed args: {0}".format(args.proj_name))
-
-    if len(args.proj_name):
-        projs = args.proj_name
-    else:
-        print("using default projs: {0}".format(DEFAULT_PROJS))
-        projs = DEFAULT_PROJS
-
-    ## rootdir
-    rootdir = Path(".")
-    rootdir_abs = Path(".").absolute()
-    print("ROOT: {0}".format(rootdir_abs))
-
-    indexer = Indexer()
-    for proj_name in projs:
-        indexer.processProj(
-            proj_name,
-            proj_name,
-            SEARCH_PATHS,
-            SKIP_PATHS,
-            INCLUDE_EXTENSIONS,
-            OTHER_EXTENSIONS,
-        )
-        # processProj(proj_name + "_updater", proj_name, SEARCH_PATHS, SKIP_PATHS, INCLUDE_EXTENSIONS, OTHER_EXTENSIONS)
